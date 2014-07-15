af3d0f0cb2ac3d9e696b507dc1c59f4b1207803a
Jamie May <jamiemay@Elliots-Mac-Mini.local>
2014-07-15 11:31:22 +0100

    Initial Commit

---
+++ NetworkingApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NetworkingApp
+//
+//  Created by Jamie May on 15/07/2014.
+//  Copyright (c) 2014 James May. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetworkingApp/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  NetworkingApp
+//
+//  Created by Jamie May on 15/07/2014.
+//  Copyright (c) 2014 James May. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
